3dafe26ce79b7ae87d56a168975894242f8223a0
CheolWoong Choi <bluesky4381@naver.com>
2023-05-07 01:51:07 +0900

    test : 컴포넌트 유닛테스트 기본코드 작성

---
+++ components/layouts/PageLayout/index.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+
+import PageLayout from '.';
+
+describe('<PageLayout/>', () => {
+  it('PageLayout 텍스트를 보여준다.', () => {
+    render(<PageLayout />);
+
+    expect(screen.getByText(/PageLayout/i)).toBeInTheDocument();
+  });
+});
