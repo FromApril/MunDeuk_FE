b6ea984a081c502b5322ae3064f37471276b81d7
CheolWoong Choi <bluesky4381@naver.com>
2023-06-01 23:14:27 +0900

    fix : global 스타일 수정, tailwind 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
+import '@/styles/global.css';
+
 import {
   Hydrate,
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import { useState } from 'react';
 import { RecoilRoot } from 'recoil';
 
+import PageLayout from '@/components/layouts/PageLayout';
 import RootErrorBoundary from '@/components/layouts/RootErrorBoundary';
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -27,7 +31,13 @@ export default function App({ Component, pageProps }: AppProps) {
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
           <RecoilRoot>
-            <Component {...pageProps} />
+            <PageLayout>
+              <Component {...pageProps} />
+              <Script
+                src="https://kit.fontawesome.com/32d07c7523.js"
+                crossOrigin="anonymous"
+              />
+            </PageLayout>
           </RecoilRoot>
         </Hydrate>
         {/* <ReactQueryDevtools initialIsOpen={false} position="top-right" /> */}
