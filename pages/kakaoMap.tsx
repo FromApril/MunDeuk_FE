57b6a5469c3bc49d2ed0dd156199196b681d982d
CheolWoong Choi <bluesky4381@naver.com>
2023-06-06 22:38:45 +0900

    feat : 카카오맵 나오는지 체크

--- pages/kakaoMap.tsx
+++ pages/kakaoMap.tsx
@@ -1,26 +1,61 @@
-import { useQuery } from '@tanstack/react-query';
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 
-import client from '@/apis/client';
 import ApiErrorBoundary from '@/components/layouts/ApiErrorBoundary';
+import { LatLng } from '@/interfaces/naverMap';
 
 export default function KakaoMapPage() {
   return (
     <main>
       <ApiErrorBoundary>
         <Suspense fallback={<div>loading...</div>}>
-          <Map />
+          <KakaoMap />
         </Suspense>
       </ApiErrorBoundary>
     </main>
   );
 }
 
-function Map() {
-  const { data } = useQuery(['/kakaoMap'], () => client.get('/hi'), {
-    suspense: true,
-    useErrorBoundary: true,
+function KakaoMap() {
+  const [latLng, setLatLng] = useState<LatLng>({
+    lat: -9999,
+    lng: -9999,
   });
 
-  return <section>asdf</section>;
+  const isLoading = latLng.lat < 0 && latLng.lng < 0;
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position.coords.latitude, position.coords.longitude);
+
+        const { latitude, longitude } = position.coords;
+
+        setLatLng({
+          lat: latitude,
+          lng: longitude,
+        });
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      window.kakao.maps.load(() => {
+        const container = document.getElementById('map');
+        const options = {
+          center: new window.kakao.maps.LatLng(latLng.lat, latLng.lng),
+          level: 3,
+        };
+
+        const map = new window.kakao.maps.Map(container, options);
+      });
+    }
+  }, [latLng]);
+
+  return (
+    <div>
+      {isLoading && <div>Loading...</div>}
+      <div id="map" style={{ width: 400, height: 400 }} />
+    </div>
+  );
 }
