8627573575f2ff354c92accf010ceced2d193df6
CheolWoong Choi <bluesky4381@naver.com>
2023-06-16 18:47:09 +0900

    feat : 홈 페이지에 카카오맵 적용 - 현재 위치를 기반으로 지도를 보여주기

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,5 +1,33 @@
-import React from 'react';
+import styled from '@emotion/styled';
+import React, { useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+
+import { userAtom } from '@/recoil/user/atoms';
+import { layouts } from '@/styles/layouts';
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const { location } = useRecoilValue(userAtom);
+  const { latitude, longitude } = location;
+  const isLoading = latitude < 0 && longitude < 0;
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    window.kakao.maps.load(() => {
+      const container = document.getElementById('map');
+      const options = {
+        center: new window.kakao.maps.LatLng(latitude, longitude),
+        level: 3,
+      };
+
+      new window.kakao.maps.Map(container, options);
+    });
+  }, [location]);
+
+  return <MainMap id="map" />;
 }
+
+// styled
+const MainMap = styled.div`
+  height: calc(100vh - ${layouts.gnb});
+`;
