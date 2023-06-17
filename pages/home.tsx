fe6f2fbf616701d64849654230168dd8dbdaa0ce
CheolWoong Choi <bluesky4381@naver.com>
2023-06-17 16:12:10 +0900

    feat : 홈 페이지에 카카오맵 지도 반영

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,27 +1,18 @@
 import styled from '@emotion/styled';
-import React, { useEffect } from 'react';
-import { useRecoilValue } from 'recoil';
+import { useEffect } from 'react';
 
-import { userAtom } from '@/recoil/user/atoms';
+import useKakaoMap from '@/hooks/useKakaoMap';
+import useLocation from '@/hooks/useLocation';
 import { layouts } from '@/styles/layouts';
 
 export default function HomePage() {
-  const { location } = useRecoilValue(userAtom);
-  const { latitude, longitude } = location;
-  const isLoading = latitude < 0 && longitude < 0;
+  const { createMap } = useKakaoMap();
+  const { isLoading, location } = useLocation();
 
   useEffect(() => {
     if (isLoading) return;
 
-    window.kakao.maps.load(() => {
-      const container = document.getElementById('map');
-      const options = {
-        center: new window.kakao.maps.LatLng(latitude, longitude),
-        level: 3,
-      };
-
-      new window.kakao.maps.Map(container, options);
-    });
+    createMap('map', location);
   }, [location]);
 
   return <MainMap id="map" />;
