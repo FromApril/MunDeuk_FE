a39f286b1939e3eee878b202ea76b7456daa3194
CheolWoong Choi <bluesky4381@naver.com>
2023-06-29 21:04:35 +0900

    feat : 홈 페이지에 지도가 없으면 다시로드하는 버튼 추가

--- pages/home/index.page.tsx
+++ pages/home/index.page.tsx
@@ -2,12 +2,13 @@ import styled from '@emotion/styled';
 
 import MapLoading from '@/components/common/MapLoading';
 import PageContainer from '@/components/layouts/PageContainer';
+import { positionAbsoluteXYCenter } from '@/styles/common';
 import { layouts } from '@/styles/layouts';
 
 import useHomePage from './logics';
 
 export default function HomePage() {
-  const { isLoading, isError } = useHomePage();
+  const { isLoading, isError, isHasMapImage, mapRef, initMap } = useHomePage();
 
   if (isLoading || isError) {
     return <MapLoading />;
@@ -15,11 +16,21 @@ export default function HomePage() {
 
   return (
     <PageContainer>
-      <Map id="map" />
+      <Map ref={mapRef} id="map" />
+      {!isHasMapImage && (
+        <ReloadButton onClick={initMap}>맵 다시 불러오기</ReloadButton>
+      )}
     </PageContainer>
   );
 }
 
 const Map = styled.div`
+  position: relative;
   height: calc(100vh - ${layouts.gnb});
+`;
+
+const ReloadButton = styled.button`
+  ${positionAbsoluteXYCenter};
+  background-color: #ddd;
+  padding: 5px;
 `;
