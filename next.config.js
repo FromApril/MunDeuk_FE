c22ed945fce6d1f0d68555fe4e38c651d09a6830
CheolWoong Choi <bluesky4381@naver.com>
2023-07-01 18:43:31 +0900

    fix : CI 단계에서 Next.js rewrite 오류 발생 & rewrite 설정 제거

--- next.config.js
+++ next.config.js
@@ -17,14 +17,6 @@ const nextConfig = {
       },
     ];
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: process.env.NEXT_PUBLIC_BASE_URL + '/:path*',
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
