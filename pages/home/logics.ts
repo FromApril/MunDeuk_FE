ae3794af0b9e293826be0871b680b97e51c8fea6
CheolWoong Choi <bluesky4381@naver.com>
2023-07-01 14:16:12 +0900

    test : 홈 페이지 지도에 마커 이미지 적용 테스트

--- pages/home/logics.ts
+++ pages/home/logics.ts
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import useNotes from '@/hooks/queries/useNotes';
 import useKakaoMap from '@/hooks/useKakaoMap';
@@ -10,6 +10,7 @@ export default function useHomePage() {
   const router = useRouter();
   const mapRef = useRef<HTMLDivElement>(null);
   const mapInstanceRef = useRef<any>();
+  const [isInitMap, setIsInitMap] = useState(false);
   const { isLoading, isError, location } = useLocation();
   const { createMap, createMarker } = useKakaoMap();
   const { data: notes } = useNotes();
@@ -18,14 +19,6 @@ export default function useHomePage() {
 
   const goNotePickPage = (id: number) => {
     router.push(`/note/pick?id=${id}`);
-  };
-
-  const addMarkerClickEvent = (marker: any) => {
-    window.kakao.maps.event.addListener(marker, 'click', function () {
-      const id = marker.getTitle();
-
-      goNotePickPage(id);
-    });
   };
 
   const initMap = () => {
@@ -39,13 +32,35 @@ export default function useHomePage() {
   };
 
   const initMarkers = (notes: Note[]) => {
+    console.log('initMarkers');
+
     const map = mapInstanceRef.current;
 
-    notes.forEach((note) => {
-      const { latitude: lat, longitude: long, id } = note;
-
-      // test
+    notes.forEach((note, idx) => {
+      // const { latitude, longitude, id } = note;
+      const { id } = note;
       const { latitude, longitude } = location;
+
+      const imageType =
+        idx % 4 === 0
+          ? 'depressed.svg'
+          : idx % 4 === 1
+          ? 'flutter.svg'
+          : idx % 4 === 2
+          ? 'glad.svg'
+          : idx % 4 === 3
+          ? 'touched.svg'
+          : '';
+
+      const imageSrc = window.location.origin + '/images/' + imageType;
+      const imageSize = new window.kakao.maps.Size(30, 30);
+      const imageOption = { offset: new window.kakao.maps.Point(30, 30) };
+
+      const markerImage = new window.kakao.maps.MarkerImage(
+        imageSrc,
+        imageSize,
+        imageOption,
+      );
 
       const marker = createMarker({
         location: {
@@ -53,10 +68,19 @@ export default function useHomePage() {
           longitude: longitude + Math.random() * 0.001,
         },
         title: id,
+        image: markerImage,
       });
 
       addMarkerClickEvent(marker);
       marker.setMap(map);
+    });
+  };
+
+  const addMarkerClickEvent = (marker: any) => {
+    window.kakao.maps.event.addListener(marker, 'click', function () {
+      const id = marker.getTitle();
+
+      goNotePickPage(id);
     });
   };
 
@@ -66,15 +90,16 @@ export default function useHomePage() {
     if (mapInstanceRef.current) return;
 
     initMap();
+    setIsInitMap(true);
   }, [location]);
 
   // 맵 초기화 & 쪽지리스트 API 성공 후, 마커 삽입
   useEffect(() => {
-    if (!mapInstanceRef.current) return;
     if (!notes) return;
+    if (!isInitMap) return;
 
     initMarkers(notes);
-  }, [mapInstanceRef.current, notes]);
+  }, [isInitMap, notes]);
 
   return {
     mapRef,
