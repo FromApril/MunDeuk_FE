845acd86f2f7e1db547a2c1748cee08851cb76d3
CheolWoong Choi <bluesky4381@naver.com>
2023-07-04 16:22:20 +0900

    fix : GNB 세로 크기 & 아이콘 크기 조정

--- components/layouts/Gnb/index.tsx
+++ components/layouts/Gnb/index.tsx
@@ -1,16 +1,10 @@
 import styled from '@emotion/styled';
 import { useRouter } from 'next/router';
-import React from 'react';
+import { memo } from 'react';
 
 import { layouts } from '@/styles/layouts';
 
-// TODOS
-// 1. 라우팅 페이지는 임의로 지정
-// 2. 아이콘은 커스텀하게 바뀌어야 할듯
-// 3. 전반적인 스타일 조정될 수 있음
-// 4. GNB SHOW/HIDE 처리가 상위 컴포넌트에서 이뤄져야함 (메인페이지에서만 GNB SHOW, 나머지 HIDE) 상태체크필요
-
-export default function Gnb() {
+function Gnb() {
   const router = useRouter();
 
   const goPage = (pathname: '/home' | '/noteWrite?page=1' | '/myPage') => {
@@ -21,21 +15,21 @@ export default function Gnb() {
     <StyledGnb>
       <div>
         <i
-          className="fa-solid fa-house"
+          className="fa-solid fa-house fa-lg"
           aria-label="홈"
           onClick={() => goPage('/home')}
         />
       </div>
       <div>
         <i
-          className="fa-solid fa-plus"
+          className="fa-solid fa-plus fa-lg"
           aria-label="쪽지작성"
           onClick={() => goPage('/noteWrite?page=1')}
         />
       </div>
       <div>
         <i
-          className="fa-solid fa-user"
+          className="fa-solid fa-user fa-lg"
           aria-label="마이페이지"
           onClick={() => goPage('/myPage')}
         />
@@ -43,6 +37,8 @@ export default function Gnb() {
     </StyledGnb>
   );
 }
+
+export default memo(Gnb);
 
 const StyledGnb = styled.nav`
   background-color: #ddd;
