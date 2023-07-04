ad2ac12d752442cff9929626a83501b3d33216e6
CheolWoong Choi <bluesky4381@naver.com>
2023-07-04 17:00:22 +0900

    fix : 쪽지 습득했을 때 감정 이미지가 안 나오는 오류 수정 - 쪽지(Note) Id 타입이 number인지 string인지 명확하지 않다, 재정의 필요

--- pages/note/pick/logics.ts
+++ pages/note/pick/logics.ts
@@ -6,7 +6,7 @@ export default function useNotePickPage() {
   const router = useRouter();
   const { data: notes } = useNotes();
   const noteId = Number(router.query.id);
-  const note = notes?.find((note) => note.id === noteId);
+  const note = notes?.find((note) => note.id == noteId);
 
   const goHomePage = () => router.push('/home');
 
