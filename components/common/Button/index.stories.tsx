c322b20af0688133d25fb391f828d2a6b81929ab
CheolWoong Choi <bluesky4381@naver.com>
2023-07-05 15:20:20 +0900

    feat : 버튼 컴포넌트에 대한 스토리 추가

---
+++ components/common/Button/index.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Button from './index';
+
+const meta: Meta<typeof Button> = {
+  title: 'Components/Common/Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+    children: 'Primary'
+  }
+};
+
+export const Dashed: Story = {
+  args: {
+    variant: 'dashed',
+    children: 'Dashed'
+  }
+}
+
+export const Disabled: Story = {
+  args: {
+    ...Primary.args,
+    children: 'Disabled',
+    disabled: true,
+  }
+}
