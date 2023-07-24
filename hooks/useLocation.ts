95f3cb1fd49945b23ede34764a9d2adede2bd1a9
CheolWoong Choi <bluesky4381@naver.com>
2023-07-24 13:25:05 +0900

    feat : UseLocation 커스텀 훅에 위치정보 체크하는 함수 추가

--- hooks/useLocation.ts
+++ hooks/useLocation.ts
@@ -1,17 +1,25 @@
 import { useRecoilValue } from 'recoil';
 
+import { Location } from '@/interfaces/common';
 import { userAtom } from '@/recoil/user/atoms';
 
 export default function useLocation() {
   const { location } = useRecoilValue(userAtom);
   const { latitude, longitude } = location;
 
-  const isLoading = latitude === 0 && longitude === 0;
+  const isEmptyLocation = (location: Location) => {
+    const { latitude, longitude } = location;
+
+    return latitude === 0 && longitude === 0;
+  };
+
+  const isLoading = isEmptyLocation(location);
   const isError = latitude === -1 && longitude === -1;
 
   return {
     location,
     isLoading,
     isError,
+    isEmptyLocation,
   };
 }
