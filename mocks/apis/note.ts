08161985354183870773d440e3fa56500513b07d
CheolWoong Choi <bluesky4381@naver.com>
2023-08-02 12:23:16 +0900

    fix : msw 모킹 API 핸들러 수정

--- mocks/apis/note.ts
+++ mocks/apis/note.ts
@@ -1,7 +1,9 @@
 import { rest } from 'msw';
 
+import { BASE_URL } from '../common';
+
 const handlers = [
-  rest.post('/note/withImage', (req, res, ctx) => {
+  rest.post(BASE_URL + '/note/withImage', (req, res, ctx) => {
     return res(ctx.status(200));
   }),
 ];
--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,29 +1,3 @@
-import { rest } from 'msw';
+import noteHandlers from './apis/note';
 
-export const handlers = [
-  rest.post('/login', (req, res, ctx) => {
-    sessionStorage.setItem('is-authenticated', 'true');
-
-    return res(ctx.status(200));
-  }),
-
-  rest.get('/user', (req, res, ctx) => {
-    const isAuthenticated = sessionStorage.getItem('is-authenticated');
-
-    if (!isAuthenticated) {
-      return res(
-        ctx.status(403),
-        ctx.json({
-          errorMessage: 'Not authorized',
-        }),
-      );
-    }
-
-    return res(
-      ctx.status(200),
-      ctx.json({
-        username: 'admin',
-      }),
-    );
-  }),
-];
+export const handlers = [...noteHandlers];
