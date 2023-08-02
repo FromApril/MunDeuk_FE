f02d1b6f3971796d0a1bc24f3a8fe67c389b47d6
CheolWoong Choi <bluesky4381@naver.com>
2023-08-02 12:22:16 +0900

    fix : 테스트 유틸함수 수정

--- tests/util.tsx
+++ tests/util.tsx
@@ -1,8 +1,11 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { RecoilRoot } from 'recoil';
+import { MutableSnapshot, RecoilRoot, RecoilState } from 'recoil';
 
-export function withRecoil(children: React.ReactNode) {
-  return <RecoilRoot>{children}</RecoilRoot>;
+export function withRecoilRoot(
+  children: React.ReactNode,
+  initializeState: (mutableSnapshot: MutableSnapshot) => void,
+) {
+  return <RecoilRoot initializeState={initializeState}>{children}</RecoilRoot>;
 }
 
 export function withQueryClient(children: React.ReactNode) {
@@ -11,6 +14,12 @@ export function withQueryClient(children: React.ReactNode) {
   return (
     <QueryClientProvider client={testClient}>{children}</QueryClientProvider>
   );
+}
+
+export function getInitializeState<T>(atom: RecoilState<T>, value: T) {
+  return function ({ set }: MutableSnapshot) {
+    set(atom, value);
+  };
 }
 
 function createTestQueryClient() {
