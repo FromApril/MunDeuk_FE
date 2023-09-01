b5235be8e784fa7f498878267b0b1a2752e34929
CheolWoong Choi <bluesky4381@naver.com>
2023-09-01 12:46:30 +0900

    fix : 에러바운더리의 상위 DOM 요소를 변경

--- components/layouts/RootErrorBoundary/index.tsx
+++ components/layouts/RootErrorBoundary/index.tsx
@@ -1,18 +1,20 @@
 import { PropsWithChildren } from 'react';
 import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
 
+import { StyledPageLayout } from '../PageLayout';
+
 type RootErrorBoundaryProps = PropsWithChildren;
 
 function Fallback({ error, resetErrorBoundary }: FallbackProps) {
   return (
-    <div>
+    <StyledPageLayout>
       <h1>일시적인 오류가 발생했습니다</h1>
       <div>
         <button type="button" onClick={resetErrorBoundary}>
           다시 시도하기
         </button>
       </div>
-    </div>
+    </StyledPageLayout>
   );
 }
 
